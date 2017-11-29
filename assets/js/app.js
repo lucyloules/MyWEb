1d39f701334985339335d275c43e5ddc68ee1657
Lucyloules <lucia.lagos@gmail.com>
2017-11-29 11:20:06 -0300

    nuevo repositorio por falla, se agrega el grid y las imagenes

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/*window.onload = function(){
+var slider = {
+	_images:[
+	"img/austin-fireworks.jpg",
+	"ankorr-wat.jpg",
+	]
+	_current:0,
+	next:function(){
+		if(++slider._current == slider._images.length){
+			slider._current = 0;
+		}
+		document.getElementById("slider").src = slider._images[slider._current];
+	},
+	prev:function(){
+		if (--slider._current < 0) {
+			slider._current = slider._images.length -1;
+		}
+		document.getElementById("slider").src = slider._images[slider._current];
+	}
+};
+
+document.getElementById("next").addEventListener("click",slider.next);
+document.getElementById("prev").addEventListener("click",slider.prev);
+
+}
+
+
+*/
+
